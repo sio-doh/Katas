6bf98d7629512d208ca0cbb3bd1f2fca3768ec4d
Siobhan Doherty <siobhandoherty18@gmail.com>
2022-08-07 23:41:56 +0200

    multiply kata

---
+++ 8kyu/multiply.js
@@ -0,0 +1,9 @@
+/*
+https://www.codewars.com/kata/50654ddff44f800200000004
+This code does not execute properly. Try to figure out why. 
+*/
+function multiply(a, b){
+    return a * b
+}
+
+multiply(100, 200)
