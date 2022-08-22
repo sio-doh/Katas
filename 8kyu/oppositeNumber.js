b1ee89ccd2d5daecf30d8a96b52e7c19412998eb
Siobhan Doherty <siobhandoherty18@gmail.com>
2022-08-22 20:53:33 +0200

    oppositeNumber kata

---
+++ 8kyu/oppositeNumber.js
@@ -0,0 +1,10 @@
+/*
+https://www.codewars.com/kata/56dec885c54a926dcd001095 
+DESCRIPTION:
+Very simple, 
+given an integer or a floating-point number, 
+find its opposite.
+*/ 
+function opposite(number) {
+    return (-number);
+}
