17a2bdb7bcd9898f163565e09c5ab3c2c2979829
Siobhan Doherty <siobhandoherty18@gmail.com>
2022-08-23 23:20:33 +0200

    booleanToString kata

---
+++ 8kyu/boolToStr.js
@@ -0,0 +1,12 @@
+/* 
+https://www.codewars.com/kata/56dec885c54a926dcd001095
+DESCRIPTION:
+Very simple, given an integer or a floating-point number, find its opposite.
+Examples:
+1: -1
+14: -14
+-34: 34
+*/ 
+function booleanToString(b){
+    return Boolean(b) ? "true" : "false";
+}
