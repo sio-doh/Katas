11ce53f61e2e4c65e9df4669812f0f41fe43e94c
Siobhan Doherty <siobhandoherty18@gmail.com>
2022-08-25 21:45:59 +0200

    stringToNumber kata

---
+++ 8kyu/strToNum.js
@@ -0,0 +1,11 @@
+/*
+https://www.codewars.com/kata/544675c6f971f7399a000e79
+Description
+We need a function that can transform a string into a number. 
+What ways of achieving this do you know?
+Note: Don't worry, all inputs will be strings, 
+and every string is a perfectly valid representation of an integral number.
+*/
+const stringToNumber = function(str){
+    return str*1;
+}
