2c7e58fb7ff3ca10c14d29708b205080adc575c3
Siobhan Doherty <siobhandoherty18@gmail.com>
2022-10-12 23:29:03 +0200

    who likes it? kata

---
+++ 6kyu/likes.js
@@ -0,0 +1,35 @@
+/*
+https://www.codewars.com/kata/5266876b8f4bf2da9b000362 
+DESCRIPTION:
+You probably know the "like" system from Facebook and other pages. People can "like" blog posts, 
+pictures or other items. We want to create the text that should be displayed next to such an item.
+Implement the function which takes an array containing the names of people that like an item. 
+It must return the display text as shown in the examples:
+    []                                -->  "no one likes this"
+    ["Peter"]                         -->  "Peter likes this"
+    ["Jacob", "Alex"]                 -->  "Jacob and Alex like this"
+    ["Max", "John", "Mark"]           -->  "Max, John and Mark like this"
+    ["Alex", "Jacob", "Mark", "Max"]  -->  "Alex, Jacob and 2 others like this"
+Note: For 4 or more names, the number in "and 2 others" simply increases.
+*/ 
+function likes(names) {
+    names = names || []; 
+    switch (names.length) {
+        case 0: 
+          return "no one likes this";
+          break;
+        case 1: 
+          return names[0] + " likes this";
+          break;      
+        case 2: 
+          return names[0] + " and " + names[1] + " like this";
+          break;
+        case 3:
+          return names[0] + ", " + names[1] + " and " + names[2] + " like this"; 
+          break;
+        default:
+          return names[0] + ", " + names[1] + " and " + (names.length-2) + " others like this"; 
+          break;
+    }
+}
+
