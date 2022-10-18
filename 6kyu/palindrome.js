5a979d4cb2d9007f71a6d6209dbae0617be43b3f
Siobhan Doherty <siobhandoherty18@gmail.com>
2022-10-18 23:19:46 +0200

    permuteAPalindrome kata

---
+++ 6kyu/palindrome.js
@@ -0,0 +1,19 @@
+/* 
+https://www.codewars.com/kata/58ae6ae22c3aaafc58000079
+DESCRIPTION:
+Write a function that will check whether ANY permutation of the characters of 
+the input string is a palindrome. Bonus points for a solution that is efficient 
+and/or that uses only built-in language functions. Deem yourself brilliant if 
+you can come up with a version that does not use any function whatsoever.
+Example
+    madam -> True
+    adamm -> True
+    junk -> False
+Hint
+The brute force approach would be to generate all the permutations of the string 
+and check each one of them whether it is a palindrome. However, an optimized 
+approach will not require this at all.
+*/ 
+function permuteAPalindrome(input) { 
+    return input == input.split("").reverse().join("") ? true : false;
+}
