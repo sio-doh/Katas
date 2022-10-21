054a806125a5609bdfb28a8f9fdae417eba6fb09
Siobhan Doherty <siobhandoherty18@gmail.com>
2022-10-21 19:54:03 +0200

    remove every other element kata

---
+++ 8kyu/removingElements.js
@@ -0,0 +1,17 @@
+/* 
+https://www.codewars.com/kata/5769b3802ae6f8e4890009d2 
+DESCRIPTION:
+    Take an array and remove every second element from the array. 
+    Always keep the first element and start removing with the next element.
+Example:
+    ["Keep", "Remove", "Keep", "Remove", "Keep", ...] 
+        --> 
+    ["Keep", "Keep", "Keep", ...]
+None of the arrays will be empty, so you don't have to worry about that!
+*/
+function removeEveryOther(arr) {
+    for (let i = 1; i < arr.length; i++) {
+        arr.splice(i, 1);
+    }
+    return arr;
+}
