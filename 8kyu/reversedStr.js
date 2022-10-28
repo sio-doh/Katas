ade178d7a9d1f02ae2c1213e3cc5b6a9305aa29d
Siobhan Doherty <siobhandoherty18@gmail.com>
2022-10-28 17:57:41 +0200

    reversed strings kata

---
+++ 8kyu/reversedStr.js
@@ -0,0 +1,9 @@
+/*
+https://www.codewars.com/kata/5168bb5dfe9a00b126000018
+Complete the solution so that it reverses the string passed into it.
+    'world'  =>  'dlrow'
+    'word'   =>  'drow'
+*/
+function solution(str) {
+    return str.split("").reverse().join("");
+}
