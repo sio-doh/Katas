c01967243d39b65d83b56926b569c73e40374a2f
Siobhan Doherty <siobhandoherty18@gmail.com>
2022-10-28 19:15:36 +0200

    you cannot code under pressure #1 kata

---
+++ 8kyu/doubleInteger.js
@@ -0,0 +1,9 @@
+/*
+https://www.codewars.com/kata/53ee5429ba190077850011d4
+DESCRIPTION: 
+Code as fast as you can! 
+You need to double the integer and return it.
+*/ 
+function doubleInteger(i) {
+    return i*2;
+}
