b3f4981389a8a4889adfb7b8039d605f5154e2a3
Siobhan Doherty <siobhandoherty18@gmail.com>
2022-10-30 21:44:07 +0100

    Beginner - Lost without a map kata

---
+++ 8kyu/maps.js
@@ -0,0 +1,10 @@
+/*
+https://www.codewars.com/kata/57f781872e3d8ca2a000007e
+DESCRIPTION: 
+Given an array of integers, return a new array with each value doubled.
+For example:
+    [1, 2, 3] --> [2, 4, 6] 
+*/
+function maps(x){
+    return x.map((num) => num * 2);
+}
