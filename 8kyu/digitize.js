5a6bd870ae6969809363fd85fd1ef2b9b296c403
Siobhan Doherty <siobhandoherty18@gmail.com>
2022-10-30 20:37:47 +0100

    convert number to reversed array of digits kata

---
+++ 8kyu/digitize.js
@@ -0,0 +1,14 @@
+/*
+https://www.codewars.com/kata/5583090cbe83f4fd8c000051
+DESCRIPTION: 
+Convert number to reversed array of digits
+Given a random non-negative number, you have to return the digits of 
+this number within an array in reverse order.
+Example(Input => Output):
+    35231 => [1,3,2,5,3]
+    0 => [0]
+*/ 
+function digitize(n) {
+    let str = String(n)
+    return str.split("").map(Number).reverse();
+}
