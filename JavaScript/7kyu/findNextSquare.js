01a846b43f1b0aa9538fb131c55aeab716e5d49b
Siobhan Doherty <siobhandoherty18@gmail.com>
2022-11-04 21:05:45 +0100

    Find the next perfect square! kata

---
+++ JavaScript/7kyu/findNextSquare.js
@@ -0,0 +1,21 @@
+/*
+https://www.codewars.com/kata/56269eb78ad2e4ced1000013
+DESCRIPTION: 
+You might know some pretty large perfect squares. But what 
+about the NEXT one?
+Complete the findNextSquare method that finds the next 
+integral perfect square after the one passed as a parameter. 
+Recall that an integral perfect square is an integer n such 
+that sqrt(n) is also an integer.
+If the parameter is itself not a perfect square then -1 
+should be returned. You may assume the parameter is 
+non-negative.
+Examples:(Input --> Output)
+    121 --> 144
+    625 --> 676
+    114 --> -1 since 114 is not a perfect square
+*/ 
+function findNextSquare(sq) {
+    let x = Math.round(Math.sqrt(sq)); 
+    return x * x == sq ? (x + 1) * (x + 1) : -1; 
+}
