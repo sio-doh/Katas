bff46ee9096601b3896c35ddbd12ab83348c774f
Siobhan Doherty <siobhandoherty18@gmail.com>
2022-11-15 22:05:26 +0100

    Grasshopper - Personalised Message kata

---
+++ JavaScript/8kyu/greet.js
@@ -0,0 +1,16 @@
+/*
+https://www.codewars.com/kata/5772da22b89313a4d50012f7
+DESCRIPTION: 
+Create a function that gives a personalized greeting. This 
+function takes two parameters: name and owner.
+Use conditionals to return the proper message:
+        case	            return
+    name equals owner	'Hello boss'
+    otherwise	        'Hello guest'
+*/
+function greet(name, owner) {
+    if (name == owner) {
+        return "Hello boss";
+    } 
+    return "Hello guest";   
+}
