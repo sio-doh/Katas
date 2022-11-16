d97a37230a4402f752e59855c10a7d6f29a39200
Siobhan Doherty <siobhandoherty18@gmail.com>
2022-11-16 16:45:46 +0100

    Convert boolean values to strings 'Yes' or 'No' kata

---
+++ Python/8kyu/bool_to_word.py
@@ -0,0 +1,7 @@
+# https://www.codewars.com/kata/53369039d7ab3ac506000467
+# DESCRIPTION: 
+# Complete the method that takes a boolean value and return 
+# a "Yes" string for true, or a "No" string for false.
+def bool_to_word(boolean):
+    if boolean is True: return "Yes"
+    else: return "No"
