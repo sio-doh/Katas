60af457a61ec900d716ccfc51487121159bf3e27
Siobhan Doherty <siobhandoherty18@gmail.com>
2022-11-17 19:37:32 +0100

    Count characters in your string kata

---
+++ Python/6kyu/count.py
@@ -0,0 +1,9 @@
+# https://www.codewars.com/kata/52efefcbcdf57161d4000091
+# DESCRIPTION 
+# The main idea is to count all the occurring characters 
+# in a string. If you have a string like aba, then the 
+# result should be {'a': 2, 'b': 1}.
+# What if the string is empty? Then the result should 
+# be empty object literal, {}.
+def count(string):
+    return { c:string.count(c) for c in string }
