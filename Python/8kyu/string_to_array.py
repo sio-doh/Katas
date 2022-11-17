d5ec06cc42e7971f382883d76993ae3df89b3176
Siobhan Doherty <siobhandoherty18@gmail.com>
2022-11-17 19:45:07 +0100

    Convert a string to an array kata

---
+++ Python/8kyu/string_to_array.py
@@ -0,0 +1,11 @@
+# https://www.codewars.com/kata/57e76bc428d6fbc2d500036d
+# DESCRIPTION 
+# Write a function to split a string and 
+# convert it into an array of words.
+# Examples (Input ==> Output):
+#     "Robin Singh" ==> ["Robin", "Singh"]
+
+#     "I love arrays they are my favorite" ==> 
+#     ["I", "love", "arrays", "they", "are", "my", "favorite"] 
+def string_to_array(s): 
+    return s.split(" ")
