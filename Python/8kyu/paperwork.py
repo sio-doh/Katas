abbc95ea3b5f62c51fe8abdf16b8378b104b614f
Siobhan Doherty <siobhandoherty18@gmail.com>
2022-11-30 19:21:58 +0100

    Beginner Series #1 School Paperwork kata

---
+++ Python/8kyu/paperwork.py
@@ -0,0 +1,14 @@
+# https://www.codewars.com/kata/55f9b48403f6b87a7c0000bd
+# DESCRIPTION 
+# Your classmates asked you to copy some paperwork for them. 
+# You know that there are 'n' classmates and the paperwork 
+# has 'm' pages.
+# Your task is to calculate how many blank pages do you need. 
+# If n < 0 or m < 0 return 0.
+# Example:
+# n= 5, m=5: 25
+# n=-5, m=5:  0
+# Waiting for translations and Feedback! Thanks!
+
+def paperwork(n, m):
+    return (0 if n < 0 or m < 0 else n*m)
