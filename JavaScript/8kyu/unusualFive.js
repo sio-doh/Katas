dcc88b88fe3851795734c50c0f80b6eb09ae5f73
Siobhan Doherty <siobhandoherty18@gmail.com>
2022-12-14 14:53:35 +0100

    5 without numbers!!

---
+++ JavaScript/8kyu/unusualFive.js
@@ -0,0 +1,10 @@
+/*
+https://www.codewars.com/kata/59441520102eaa25260000bf
+DESCRIPTION: 
+Write a function that always returns 5
+Sounds easy right? Just bear in mind that you can't 
+use any of the following characters: 0123456789*+-/
+*/
+function unusualFive() { 
+    return "marco".length;
+}
