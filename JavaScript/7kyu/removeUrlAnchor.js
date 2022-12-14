62d3631b67b3234718aad78ab1f889423374ea8f
Siobhan Doherty <siobhandoherty18@gmail.com>
2022-12-14 16:14:53 +0100

    Remove anchor from URL kata

---
+++ JavaScript/7kyu/removeUrlAnchor.js
@@ -0,0 +1,12 @@
+/*
+https://www.codewars.com/kata/51f2b4448cadf20ed0000386
+DESCRIPTION: 
+Complete the function/method so that it returns the 
+url with anything after the anchor (#) removed.
+Examples
+    "www.codewars.com#about" --> "www.codewars.com"
+    "www.codewars.com?page=1" -->"www.codewars.com?page=1"
+*/
+function removeUrlAnchor(url){
+    return url.replace(/#.*/, "");
+} 
