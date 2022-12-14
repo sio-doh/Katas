eebfa203dd3fd61d9518a5762e001f67cdc396fc
Siobhan Doherty <siobhandoherty18@gmail.com>
2022-12-14 14:31:35 +0100

    Hex to Decimal kata

---
+++ JavaScript/8kyu/hexToDec.js
@@ -0,0 +1,9 @@
+/*
+https://www.codewars.com/kata/57a4d500e298a7952100035d
+DESCRIPTION: 
+Complete the function which converts hex number 
+(given as a string) to a decimal number.
+*/
+function hexToDec(hexString) {
+    return parseInt(hexString, 16);
+}
