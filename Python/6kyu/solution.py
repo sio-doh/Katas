3d89ecbe637eaca63b6457efe1f671b42d5c897d
Siobhan Doherty <siobhandoherty18@gmail.com>
2022-12-29 12:56:06 +0000

    Multiples of 3 or 5 kata

---
+++ Python/6kyu/solution.py
@@ -0,0 +1,20 @@
+# https://www.codewars.com/kata/514b92a657cdc65150000006
+# DESCRIPTION: 
+# If we list all the natural numbers below 10 that are 
+# multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of 
+# these multiples is 23.
+# Finish the solution so that it returns the sum of all 
+# the multiples of 3 or 5 below the number passed in. 
+# Additionally, if the number is negative, return 0 (for 
+# languages that do have them).
+# Note: If the number is a multiple of both 3 and 5, 
+# only count it once.
+# Courtesy of projecteuler.net 
+# (Problem 1) https://projecteuler.net/problem=1
+
+def solution(number):
+    sum = 0 
+    for i in range(1, number): 
+        if i % 3 == 0 or i % 5 == 0: 
+            sum += i 
+    return sum
