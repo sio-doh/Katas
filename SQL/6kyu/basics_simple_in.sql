bfa7d24963c5911ba51176a8131200ca91d93373
Siobhan Doherty <siobhandoherty18@gmail.com>
2022-12-31 21:07:34 +0100

    SQL Basics: Simple IN kata

---
+++ SQL/6kyu/basics_simple_in.sql
@@ -0,0 +1,28 @@
+-- https://www.codewars.com/kata/58113c03009b4fcc66000d29
+-- DESCRIPTION:
+-- For this challenge you need to create a SELECT 
+-- statement, this SELECT statement will use an IN to 
+-- check whether a department has had a sale with a price 
+-- over 98.00 dollars.
+-- departments table schema
+--     id
+--     name
+-- sales table schema
+--     id
+--     department_id (department foreign key)
+--     name
+--     price
+--     card_name
+--     card_number
+--     transaction_date
+-- resultant table schema
+--     id
+--     name
+-- NOTE: sometimes a department will not not contain a sale over $98 so just resubmit.
+
+SELECT id, name
+FROM departments d 
+WHERE id IN (
+    SELECT department_id 
+    FROM sales 
+    WHERE price > 98.0);
