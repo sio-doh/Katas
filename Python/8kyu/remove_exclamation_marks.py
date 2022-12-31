2a50ccff7971977fb59927613f051198608cd269
Siobhan Doherty <siobhandoherty18@gmail.com>
2022-12-31 13:53:09 +0100

    Remove exclamation marks kata

---
+++ Python/8kyu/remove_exclamation_marks.py
@@ -0,0 +1,7 @@
+# https://www.codewars.com/kata/57a0885cbb9944e24c00008e
+# DESCRIPTION: 
+# Write function RemoveExclamationMarks which 
+# removes all exclamation marks from a given string.
+
+def remove_exclamation_marks(s): 
+    return s.replace('!', '')
