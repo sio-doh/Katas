d8cc9921f113514f9395f6d59b52402691170925
Siobhan Doherty <siobhandoherty18@gmail.com>
2023-01-04 14:01:38 +0100

    Convert boolean values to strings 'Yes' string for true, or a 'No' string for false

---
+++ JavaScript/8kyu/boolToWord.js
@@ -0,0 +1,11 @@
+/*
+https://www.codewars.com/kata/53369039d7ab3ac506000467
+DESCRIPTION: 
+Complete the method that takes a boolean value and 
+return a "Yes" string for true, or a "No" string 
+for false.
+*/ 
+
+function boolToWord(bool) {
+    return bool ? "Yes" : "No";
+}
