9ea6090b1cc4e0beb409d3f00bbf4054464de470
Siobhan Doherty <siobhandoherty18@gmail.com>
2023-01-04 15:00:44 +0100

    Enumerable Magic #3 - Does My List Include This? - JS kata

---
+++ JavaScript/8kyu/enumMagic.js
@@ -0,0 +1,13 @@
+/*
+https://www.codewars.com/kata/545991b4cbae2a5fda000158
+DESCRIPTION: 
+Create a method that accepts a list and an item, and 
+returns true if the item belongs to the list, 
+otherwise false.
+*/ 
+
+// function include(arr, item) {
+//   return arr.includes(item); 
+// } 
+
+include = (arr, item) => arr.includes(item);
