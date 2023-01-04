baae471b20706fef38719678784ab16e7fe39a9e
Siobhan Doherty <siobhandoherty18@gmail.com>
2023-01-04 14:16:11 +0100

    Remove exclamation marks - JS kata

---
+++ JavaScript/8kyu/removeExclamationMarks.js
@@ -0,0 +1,12 @@
+/*
+https://www.codewars.com/kata/57a0885cbb9944e24c00008e 
+DESCRIPTION:
+Write function RemoveExclamationMarks which removes 
+all exclamation marks from a given string. 
+*/ 
+
+// function removeExclamationMarks(s) {
+//   return s.replace(/\!/g,''); 
+// }
+
+removeExclamationMarks = s => s.replace(/\!/g,'');
