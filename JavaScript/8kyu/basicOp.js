8c5abc5ad5a15bb534a0a32adac808c8d3a660c3
Siobhan Doherty <siobhandoherty18@gmail.com>
2023-01-06 23:31:33 +0100

    Basic Mathematical Operations - JS kata

---
+++ JavaScript/8kyu/basicOp.js
@@ -0,0 +1,19 @@
+/*
+https://www.codewars.com/kata/57356c55867b9b7a60000bd7
+DESCRIPTION: 
+Your task is to create a function that does four basic 
+mathematical operations.
+The function should take three arguments - 
+operation(string/char), value1(number), value2(number).
+The function should return result of numbers after 
+applying the chosen operation.
+Examples(Operator, value1, value2) --> output
+    ('+', 4, 7) --> 11
+    ('-', 15, 18) --> -3
+    ('*', 5, 5) --> 25
+    ('/', 49, 7) --> 7
+*/
+
+function basicOp(operation, value1, value2) {
+    return eval(value1 + operation + value2);
+}
