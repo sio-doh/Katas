450a01fe66b8f224ea20502a2c8188e30791c47c
Siobhan Doherty <siobhandoherty18@gmail.com>
2023-01-08 00:16:51 +0100

    Get Nth Even Number - JS kata

---
+++ JavaScript/8kyu/nthEven.js
@@ -0,0 +1,15 @@
+/*
+https://www.codewars.com/kata/5933a1f8552bc2750a0000ed 
+DESCRIPTION:
+Return the Nth Even Number
+Example(Input --> Output)
+    1 --> 0 (the first even number is 0)
+    3 --> 4 (the 3rd even number is 4 (0, 2, 4))
+    100 --> 198
+    1298734 --> 2597466
+The input will not be 0.
+*/ 
+
+function nthEven(n) {
+    return n * 2 - 2; 
+}
