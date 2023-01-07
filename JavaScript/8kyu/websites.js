0533fb23dd5a6d3ba864593b562e6831678417cd
Siobhan Doherty <siobhandoherty18@gmail.com>
2023-01-08 00:28:49 +0100

    Kata Example Twist - JS kata

---
+++ JavaScript/8kyu/websites.js
@@ -0,0 +1,11 @@
+/*
+https://www.codewars.com/kata/525c1a07bb6dda6944000031 
+DESCRIPTION:
+This is an easy twist to the example kata (provided 
+by Codewars when learning how to create your own 
+kata).
+Add the value "codewars" to the array websites/Websites 
+1,000 times.
+*/ 
+
+var websites = new Array(1000).fill('codewars');
