6ad45789eb11c64a064a64272d5f5b74ac0735ee
Siobhan Doherty <siobhandoherty18@gmail.com>
2023-01-08 00:19:17 +0100

    Grasshopper - Variable Assignment Debug JS kata

---
+++ JavaScript/8kyu/variableAssignment.js
@@ -0,0 +1,12 @@
+/*
+https://www.codewars.com/kata/5612e743cab69fec6d000077
+DESCRIPTION:
+Variable assignment
+Fix the variables assigments so that this code 
+stores the string 'devLab' in the variable 
+name.
+*/ 
+
+var a = "dev";
+var b = "Lab";
+var name = a + b;
