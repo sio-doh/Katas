7f97de4caeb14b4c158173b68b3207b96d93d464
Siobhan Doherty <siobhandoherty18@gmail.com>
2023-01-08 00:14:23 +0100

    Grasshopper - Function syntax debugging JS kata

---
+++ JavaScript/8kyu/main.js
@@ -0,0 +1,12 @@
+/*
+https://www.codewars.com/kata/56dae9dc54c0acd29d00109a 
+DESCRIPTION:
+Grasshopper - Function syntax debugging
+A student was working on a function and made some 
+syntax mistakes while coding. Help them find their 
+mistakes and fix them. 
+*/ 
+
+function main(verb, noun) {
+    return verb + noun;
+} 
