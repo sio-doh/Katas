ce2adfed166464a4927bcffdfc12992a5eeda81e
Siobhan Doherty <siobhandoherty18@gmail.com>
2023-01-08 00:12:20 +0100

    Function 2 - squaring an argument JS kata

---
+++ JavaScript/8kyu/square.js
@@ -0,0 +1,8 @@
+/*
+https://www.codewars.com/kata/523b623152af8a30c6000027 
+DESCRIPTION:
+Now you have to write a function that takes an 
+argument and returns the square of it.
+*/ 
+
+square = arg => arg ** 2;
