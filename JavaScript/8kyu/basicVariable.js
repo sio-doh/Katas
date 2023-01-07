5ade1b914e169280036f52bd7dac1c845df6ac34
Siobhan Doherty <siobhandoherty18@gmail.com>
2023-01-08 00:31:22 +0100

    Basic variable assignment JS kata

---
+++ JavaScript/8kyu/basicVariable.js
@@ -0,0 +1,12 @@
+/*
+https://www.codewars.com/kata/50ee6b0bdeab583673000025 
+DESCRIPTION:
+This code should store "codewa.rs" as a variable 
+called name but it's not working. Can you figure 
+out why?
+*/ 
+
+var a = "code";
+var b = "wa.rs";
+var name = a + b;
+
