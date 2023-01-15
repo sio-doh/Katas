327382c22c2b797517392b504845494bc8ecc63b
Siobhan Doherty <siobhandoherty18@gmail.com>
2023-01-15 22:03:29 +0100

    Incorrect division method - JS kata

---
+++ JavaScript/8kyu/solve.js
@@ -0,0 +1,9 @@
+/*
+https://www.codewars.com/kata/54d1c59aba326343c80000e7 
+DESCRIPTION:
+This method, which is supposed to return the result 
+of dividing its first argument by its second, isn't 
+always returning correct values. Fix it.
+*/ 
+
+const solve = (x, y) => x / y;
