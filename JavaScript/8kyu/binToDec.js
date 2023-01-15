7601dca3ec5cbe7141121836b9801476d71e5b8e
Siobhan Doherty <siobhandoherty18@gmail.com>
2023-01-15 22:00:38 +0100

    Bin to Decimal - JS kata

---
+++ JavaScript/8kyu/binToDec.js
@@ -0,0 +1,8 @@
+/*
+https://www.codewars.com/kata/57a5c31ce298a7e6b7000334 
+DESCRIPTION:
+Complete the function which converts a binary number 
+(given as a string) to a decimal number. 
+*/ 
+
+binToDec = bin => parseInt(bin, 2);
