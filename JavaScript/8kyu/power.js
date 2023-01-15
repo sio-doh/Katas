057e84046b3eba8e3e0ae56df4ea9b143f9454a3
Siobhan Doherty <siobhandoherty18@gmail.com>
2023-01-15 21:58:33 +0100

    Potenciation JS kata

---
+++ JavaScript/8kyu/power.js
@@ -0,0 +1,15 @@
+/*
+https://www.codewars.com/kata/59fc72fe235f93838b002235 
+DESCRIPTION:
+The function takes in 2 inputs x and y, and should 
+return x to the power of y
+Simple, right? But you're NOT allowed to use 
+Math.pow();
+Obs: x and y will be naturals, so DON'T take fractions 
+into consideration;
+Note : zero to the power of zero equals one in this 
+kata
+Great coding <3
+*/ 
+
+power = (x, y) => x ** y;
