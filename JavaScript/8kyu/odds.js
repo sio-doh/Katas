9ab54fcbb6de8a30f45ca1fe8d71f64a458bd820
Siobhan Doherty <siobhandoherty18@gmail.com>
2023-01-15 22:35:37 +0100

    Are arrow functions odd? - JS kata

---
+++ JavaScript/8kyu/odds.js
@@ -0,0 +1,10 @@
+/*
+https://www.codewars.com/kata/559f80b87fa8512e3e0000f5 
+DESCRIPTION:
+Time to test your basic knowledge in functions! Return 
+the odds from a list:
+    [1, 2, 3, 4, 5]  -->  [1, 3, 5]
+    [2, 4, 6]        -->  []
+*/ 
+
+odds = values => values.filter(el => el % 2);
