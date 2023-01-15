25255df45c898b4a1ba9794efe5316a6c260b01b
Siobhan Doherty <siobhandoherty18@gmail.com>
2023-01-15 23:53:53 +0100

    Printing Array elements with Comma delimiters - JS kata

---
+++ JavaScript/8kyu/printArray.js
@@ -0,0 +1,13 @@
+/*
+https://www.codewars.com/kata/56e2f59fb2ed128081001328 
+DESCRIPTION:
+Input: Array of elements
+["h","o","l","a"]
+Output: String with comma delimited elements of the 
+array in th same order.
+"h,o,l,a"
+*/ 
+
+function printArray(array) {
+    return array.join();
+}
