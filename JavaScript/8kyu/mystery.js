6292c6f0cd6db2bdf2ed655a2170d99929ecebd9
Siobhan Doherty <siobhandoherty18@gmail.com>
2023-01-16 23:47:50 +0100

    Return to Sanity - JS kata

---
+++ JavaScript/8kyu/mystery.js
@@ -0,0 +1,8 @@
+/*
+https://www.codewars.com/kata/514a7ac1a33775cbb500001e
+DESCRIPTION:
+This function should return an object, but it's not 
+doing what's intended. What's wrong?
+*/ 
+
+const mystery = () => ({sanity: 'Hello'});
