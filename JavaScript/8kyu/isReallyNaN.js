2521b8e35f6345ebd25346e16830ff1d7c40249f
Siobhan Doherty <siobhandoherty18@gmail.com>
2023-01-17 11:13:46 +0100

    isReallyNaN kata

---
+++ JavaScript/8kyu/isReallyNaN.js
@@ -0,0 +1,15 @@
+/*
+https://www.codewars.com/kata/56c24c58e0c0f741d4001aef
+DESCRIPTION:
+I've hit a few bugs in my Java/Type/Coffee-script code 
+recently, and I've traced the problem back to the 
+global isNaN function I was using. I had expected it 
+to be more discerning, but it's returning true for 
+undefined right now.
+Write a function isReallyNaN that returns true only 
+if passed in an argument that evalutes to NaN, and 
+returns false otherwise.
+Any solution is acceptable!
+*/ 
+
+const isReallyNaN = (val) => Number.isNaN(val);
