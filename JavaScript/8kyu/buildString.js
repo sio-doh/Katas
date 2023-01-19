108148cd7b5a03876d415cbe7566100d3df51abe
Siobhan Doherty <siobhandoherty18@gmail.com>
2023-01-19 23:02:27 +0100

    String Templates - Bug Fixing #5

---
+++ JavaScript/8kyu/buildString.js
@@ -0,0 +1,12 @@
+/*
+https://www.codewars.com/kata/55c90cad4b0fe31a7200001f
+DESCRIPTION:
+Oh no! Timmy hasn't followed instructions very 
+carefully and forgot how to use the new String 
+Template feature, Help Timmy with his string template 
+so it works as he expects!
+*/ 
+
+function buildString(...template) {
+    return `I like ${template.join(', ')}!`;
+}
