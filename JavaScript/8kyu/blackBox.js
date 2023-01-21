62febc466ef25c4175d624bb5bc08ee9e984d827
Siobhan Doherty <siobhandoherty18@gmail.com>
2023-01-21 23:28:47 +0100

    Not so black box - JS kata

---
+++ JavaScript/8kyu/blackBox.js
@@ -0,0 +1,10 @@
+/*
+https://www.codewars.com/kata/57080f21d531cd94950007eb
+DESCRIPTION:
+The kata has been preloaded with a function called 
+blackBox. Figure out what the function does.
+You may need to think laterally!
+*/ 
+
+console.log(blackBox().toString()); 
+JSopenSesame();
