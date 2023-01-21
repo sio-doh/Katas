e19f8a1bb8e30b90e33d9a06241be4c1abb05d72
Siobhan Doherty <siobhandoherty18@gmail.com>
2023-01-21 23:16:31 +0100

    get ascii value of character - JS kata

---
+++ JavaScript/8kyu/getASCII.js
@@ -0,0 +1,11 @@
+/*
+https://www.codewars.com/kata/55acfc59c3c23d230f00006d
+DESCRIPTION:
+Get ASCII value of a character.
+For the ASCII table you can refer to 
+http://www.asciitable.com/
+*/ 
+
+function getASCII(c) {
+    return c.charCodeAt(0);
+}
