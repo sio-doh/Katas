a571098d275c53e8b527af1bb78188a7503a423a
Siobhan Doherty <siobhandoherty18@gmail.com>
2023-01-21 23:30:32 +0100

    Barking mad JS kata

---
+++ JavaScript/8kyu/dog.js
@@ -0,0 +1,22 @@
+/*
+https://www.codewars.com/kata/54dba07f03e88a4cec000caf 
+DESCRIPTION:
+Teach snoopy and scooby doo how to bark using object 
+methods. Currently only snoopy can bark and not scooby 
+doo.
+    snoopy.bark() #return "Woof"
+    scoobydoo.bark() #undefined
+Use method prototypes to enable all Dogs to bark.
+*/ 
+
+function Dog(breed) {
+    this.breed = breed;
+}
+
+var snoopy = new Dog("Beagle");
+
+Dog.prototype.bark = function() {
+    return "Woof";
+};
+
+var scoobydoo = new Dog("Great Dane");
