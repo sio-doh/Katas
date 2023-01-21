c7bd1a4e80b29f4f3758fab3d3734b1675db776c
Siobhan Doherty <siobhandoherty18@gmail.com>
2023-01-21 23:33:59 +0100

    Mr.Freeze - JS kata

---
+++ JavaScript/8kyu/freeze.js
@@ -0,0 +1,9 @@
+/*
+https://www.codewars.com/kata/514a3996d22ce03198000003
+DESCRIPTION:
+There is an object/class already created called 
+MrFreeze. Mark this object as frozen so that no other 
+changes can be made to it.
+*/ 
+
+Object.freeze(MrFreeze)
