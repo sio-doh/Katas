26703dc821886283c9db9431fe54b283a053eb7e
Siobhan Doherty <siobhandoherty18@gmail.com>
2023-01-30 19:38:48 +0100

    Ensure question - JS kata

---
+++ JavaScript/8kyu/ensureQuestion.js
@@ -0,0 +1,15 @@
+/*
+https://www.codewars.com/kata/5866fc43395d9138a7000006
+DESCRIPTION:
+Given a string, write a function that returns the 
+string with a question mark ("?") appends to the end, 
+unless the original string ends with a question mark, 
+in which case, returns the original string.
+For example (Input --> Output)
+    "Yes" --> "Yes?" 
+    "No?" --> "No?"
+*/ 
+
+function ensureQuestion(s) {
+    return s.endsWith('?') ? s : s + '?';
+}
