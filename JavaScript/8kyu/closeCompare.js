ff3046001454bb023d082c2898f44a82e85ce1c3
Siobhan Doherty <siobhandoherty18@gmail.com>
2023-01-30 19:25:51 +0100

    Compare within margin - JS kata

---
+++ JavaScript/8kyu/closeCompare.js
@@ -0,0 +1,23 @@
+/*
+https://www.codewars.com/kata/56453a12fcee9a6c4700009c
+DESCRIPTION:
+Create a function close_compare that accepts 3 
+parameters: a, b, and an optional margin. The function 
+should return whether a is lower than, close to, or 
+higher than b.
+a is considered "close to" b if margin is greater than 
+or equal to the distance between a and b.
+Please note the following:
+    When a is close to b, return 0.
+Otherwise...
+    When a is less than b, return -1.
+    When a is greater than b, return 1.
+If margin is not given, treat it as zero.
+Assume: margin >= 0
+Tip: Some languages have a way to make parameters 
+optional.
+*/ 
+
+function closeCompare(a, b, margin) { 
+    return Math.abs(a - b) <= margin ? 0 : Math.sign(a - b);
+}
