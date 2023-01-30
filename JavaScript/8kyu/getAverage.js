ddc373102f9e16f35817f9d44c84c7e479badf80
Siobhan Doherty <siobhandoherty18@gmail.com>
2023-01-30 19:50:16 +0100

    Get the mean of an array - JS kata

---
+++ JavaScript/8kyu/getAverage.js
@@ -0,0 +1,16 @@
+/*
+https://www.codewars.com/kata/563e320cee5dddcf77000158 
+DESCRIPTION:
+It's the academic year's end, fateful moment of your 
+school report. The averages must be calculated. All 
+the students come to you and entreat you to calculate 
+their average for them. Easy ! You just need to write a 
+script.
+Return the average of the given array rounded down to 
+its nearest integer.
+The array will never be empty.
+*/ 
+
+function getAverage(marks) {
+    return Math.floor(marks.reduce((a, b) => a + b) / marks.length);
+}
