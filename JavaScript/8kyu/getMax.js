bcf03731879c71957c4e9314b5224087b6bf8b6e
Siobhan Doherty <siobhandoherty18@gmail.com>
2023-01-31 15:15:42 +0100

    Max Headroom and JavaScript style - JS kata

---
+++ JavaScript/8kyu/getMax.js
@@ -0,0 +1,10 @@
+/*
+https://www.codewars.com/kata/52a47dd7e950edabfa000391
+DESCRIPTION:
+Shouldn't the two functions getMax1 and getMax2 be 
+equivalent and return the same value? Can you spot a 
+problem and fix it? Can you learn something about 
+JavaScript style in this kata?
+*/
+
+const getMax1 = getMax2 = () => ({name: 'Max Headroom'})
