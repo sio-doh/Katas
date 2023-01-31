9484ef970973d1bc7297d5773d3f375692773624
Siobhan Doherty <siobhandoherty18@gmail.com>
2023-01-31 15:27:39 +0100

    How many arguments - JS kata

---
+++ JavaScript/7kyu/argsCount.js
@@ -0,0 +1,8 @@
+/*
+https://www.codewars.com/kata/5c44b0b200ce187106452139
+DESCRIPTION:
+    args_count(1, 2, 3) -> 3
+    args_count(1, 2, 3, 10) -> 4
+*/
+
+const args_count = (...args) => args.length;
