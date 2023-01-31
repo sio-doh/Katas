2c2424233199eb362f494855227a640455dd387a
Siobhan Doherty <siobhandoherty18@gmail.com>
2023-01-31 14:33:57 +0100

    Smallest value of an array - JS kata

---
+++ JavaScript/7kyu/min.js
@@ -0,0 +1,18 @@
+/*
+https://www.codewars.com/kata/544a54fd18b8e06d240005c0
+DESCRIPTION:
+Write a function that can return the smallest value of 
+an array or the index of that value. The function's 2nd 
+parameter will tell whether it should return the value 
+or the index.
+Assume the first parameter will always be an array 
+filled with at least 1 number and no duplicates. Assume 
+the second parameter will be a string holding one of 
+two values: 'value' and 'index'.
+    min([1,2,3,4,5], 'value') // => 1
+    min([1,2,3,4,5], 'index') // => 0
+*/ 
+
+function min(arr, toReturn, minArr = Math.min(...arr)) {
+    return toReturn == "value" ? minArr : arr.indexOf(minArr);
+}
