92ffaa1f5eec47353c0b2cd9429e059d5c584d51
Siobhan Doherty <siobhandoherty18@gmail.com>
2023-01-31 15:31:35 +0100

    How many arguments - JS kata

---
+++ JavaScript/7kyu/fiveWithoutAlphanums.js
@@ -0,0 +1,11 @@
+/*
+https://www.codewars.com/kata/5a751a31373c2e645b000096
+DESCRIPTION:
+Write a function/proc/lambda called _ that always 
+returns 5.
+Sounds easy right? Just bear in mind that you can only 
+use the following characters:
+    ~!@#$%^&*()-_+=[]\{}|:;"'<,>.?/`
+*/ 
+
+_=()=>(""=="")+(""=="")+(""=="")+(""=="")+(""=="")
