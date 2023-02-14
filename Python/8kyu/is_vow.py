15416d76dcd2574cfafebd4a43a6d6f1c7cdce4d
Siobhan Doherty <siobhandoherty18@gmail.com>
2023-02-14 15:20:31 +0100

    added comment to is_vowelPY

---
+++ Python/8kyu/is_vow.py
@@ -0,0 +1,14 @@
+# https://www.codewars.com/kata/57cff961eca260b71900008f
+# DESCRIPTION:
+# Given an array of numbers, check if any of the numbers 
+# are the character codes for lower case vowels 
+# (a, e, i, o, u).
+# If they are, change the array value to a string of 
+# that vowel.
+# Return the resulting array.
+
+def is_vow(inp):
+    for val, uni in enumerate(inp):
+        if chr(uni) in "aeiou": 
+            inp[val] = chr(uni)
+    return inp
