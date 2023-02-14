682ced0afe912488adf2c3e8dce773ddf2ad643e
Siobhan Doherty <siobhandoherty18@gmail.com>
2023-02-14 15:07:08 +0100

    Character with longest consecutive repetition - JS kata

--- JavaScript/6kyu/longestRepetition.js
+++ JavaScript/6kyu/longestRepetition.js
@@ -1,4 +1,19 @@
-
+/*
+https://www.codewars.com/kata/586d6cefbcc21eed7a001155
+DESCRIPTION:
+For a given string s find the character c (or C) with 
+longest consecutive repetition and return:
+    (c, l)
+where l (or L) is the length of the repetition. If 
+there are two or more characters with the same l return 
+the first in order of appearance.
+For empty string return:
+    ('', 0)
+In JavaScript: If you use Array.sort in your solution, 
+you might experience issues with the random tests as 
+Array.sort is not stable in the Node.js version used 
+by CodeWars. This is not a kata issue.
+/*
 
 function longestRepetition(s) {
     if (s.length == 0) {
