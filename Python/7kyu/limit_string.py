f7b080fbc897e9ec8397887707ca6ff07114330c
Siobhan Doherty <siobhandoherty18@gmail.com>
2023-03-13 14:23:23 +0100

    Limit string length -1 py kata

---
+++ Python/7kyu/limit_string.py
@@ -0,0 +1,13 @@
+
+# https://www.codewars.com/kata/5208fc3cb613bc725f000142/
+# The function must return the truncated version of the given 
+#   string up to the given limit followed by "..." if the result 
+#   is shorter than the original. Return the same string if 
+#   nothing was truncated.
+# Example:
+#   solution('Testing String', 3) --> 'Tes...'
+#   solution('Testing String', 8) --> 'Testing ...'
+#   solution('Test', 8)           --> 'Test'
+
+def solution(st, limit): 
+    return st[:limit] + "..." if len(st) > limit else st
