9389d5fe0a3b2ed27c5b80c1a220e438d603c2a4
Siobhan Doherty <siobhandoherty18@gmail.com>
2023-03-22 00:06:26 +0100

    Anagram Detection - py kata content added

---
+++ Python/7kyu/is_anagram.py
@@ -0,0 +1,14 @@
+# https://www.codewars.com/kata/529eef7a9194e0cbc1000255
+# DESCRIPTION:
+# An anagram is the result of rearranging the letters of 
+# a word to produce a new word (see wikipedia).
+# Note: anagrams are case insensitive
+# Complete the function to return true if the two 
+# arguments given are anagrams of each other; return 
+# false otherwise.
+# Examples
+#     "foefet" is an anagram of "toffee"
+#     "Buckethead" is an anagram of "DeathCubeK"
+
+def is_anagram(test, original):
+    return sorted(original.lower()) == sorted(test.lower())
