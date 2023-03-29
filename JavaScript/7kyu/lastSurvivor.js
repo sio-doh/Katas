80c868b233dd3e23221c06c6afcf24822e41e4f3
Siobhan Doherty <siobhandoherty18@gmail.com>
2023-03-29 23:09:37 +0200

    Last Survivor - js kata

---
+++ JavaScript/7kyu/lastSurvivor.js
@@ -0,0 +1,27 @@
+/*
+https://www.codewars.com/kata/609eee71109f860006c377d1 
+DESCRIPTION:
+You are given a string of letters and an array of 
+numbers.
+The numbers indicate positions of letters that must be 
+removed, in order, starting from the beginning of the 
+array.
+After each removal the size of the string decreases (there 
+is no empty space).
+Return the only letter left.
+Example:
+    let str = "zbk", arr = [0, 1]
+        str = "bk", arr = [1]
+        str = "b", arr = []
+        return 'b'
+Notes
+    The given string will never be empty.
+    The length of the array is always one less than the 
+        length of the string.
+    All numbers are valid.
+    There can be duplicate letters and numbers.
+*/ 
+
+function lastSurvivor(letters, coords) {
+    return coords.reduce((a, b) => a.slice(0, b) + a.slice(b + 1), letters);
+}
