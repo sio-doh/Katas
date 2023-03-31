1210cc043781a38f6d1368188d347276cde45e97
Siobhan Doherty <siobhandoherty18@gmail.com>
2023-03-31 23:29:08 +0200

    Upper <body> Strength - js kata

---
+++ JavaScript/7kyu/alexMistakes.js
@@ -0,0 +1,26 @@
+/*
+https://www.codewars.com/kata/571640812ad763313600132b
+DESCRIPTION:
+Alex is transitioning from website design to coding and 
+wants to sharpen his skills with CodeWars.
+He can do ten kata in an hour, but when he makes a 
+mistake, he must do pushups. These pushups really tire 
+poor Alex out, so every time he does them they take twice 
+as long. His first set of redemption pushups takes 5 
+minutes. Create a function, alexMistakes, that takes two 
+arguments: the number of kata he needs to complete, and 
+the time in minutes he has to complete them. Your function 
+should return how many mistakes Alex can afford to make.
+*/
+
+function alexMistakes(numberOfKata, timeLimit) {
+    let result = 0, 
+        timeLeft = timeLimit - numberOfKata * 6, 
+        pushupsTime = 5; 
+    while (timeLeft >= pushupsTime) {
+        timeLeft -= pushupsTime; 
+        pushupsTime *= 2; 
+        result++;
+    }
+    return result;
+}
