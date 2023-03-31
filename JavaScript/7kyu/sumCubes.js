d4d5c0cedc527d90b71c4de1059d69fdbf1b2d99
Siobhan Doherty <siobhandoherty18@gmail.com>
2023-03-31 23:30:50 +0200

    Sum of Cubes - js kata

---
+++ JavaScript/7kyu/sumCubes.js
@@ -0,0 +1,21 @@
+/*
+https://www.codewars.com/kata/59a8570b570190d313000037
+DESCRIPTION:
+Write a function that takes a positive integer n, sums 
+all the cubed values from 1 to n (inclusive), and 
+returns that sum.
+Assume that the input n will always be a positive 
+integer.
+Examples: (Input --> output)
+    2 --> 9 (sum of the cubes of 1 and 2 is 1 + 8)
+    3 --> 36 (sum of the cubes of 1, 2, and 3 is 1 + 8 + 27)
+*/ 
+
+function sumCubes(n) {
+    let result = 0; 
+    while (n > 0) {
+        result += n ** 3; 
+        n--;
+    }
+    return result;
+}
