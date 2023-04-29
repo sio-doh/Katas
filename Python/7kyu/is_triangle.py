800937ec45b79ed711e96439d8dd69d13d4b494a
Siobhan Doherty <siobhandoherty18@gmail.com>
2023-04-29 23:36:18 +0200

    Is this a triangle? - py kata

---
+++ Python/7kyu/is_triangle.py
@@ -0,0 +1,14 @@
+# https://www.codewars.com/kata/56606694ec01347ce800001b
+# DESCRIPTION:
+# Implement a function that accepts 3 integer values a, b, c. 
+# The function should return true if a triangle can be 
+# built with the sides of given length and false in any other 
+# case.
+# (In this case, all triangles must have surface greater than 
+# 0 to be accepted).
+
+def is_triangle(a, b, c):
+    if a + b > c and a + c > b and b + c > a:
+        return True 
+    else: 
+        return False
