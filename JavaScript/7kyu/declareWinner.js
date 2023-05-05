25e2b69a43e97542448095bb72feabee98c1c75d
Siobhan Doherty <siobhandoherty18@gmail.com>
2023-05-05 17:24:36 +0200

    Two fighters, one winner - js kata

---
+++ JavaScript/7kyu/declareWinner.js
@@ -0,0 +1,40 @@
+// https://www.codewars.com/kata/577bd8d4ae2807c64b00045b
+// DESCRIPTION:
+// Create a function that returns the name of the winner 
+// in a fight between two fighters.
+// Each fighter takes turns attacking the other and whoever 
+// kills the other first is victorious. Death is defined 
+// as having health <= 0.
+// Each fighter will be a Fighter object/instance. See the 
+// Fighter class below in your chosen language.
+// Both health and damagePerAttack (damage_per_attack for 
+// python) will be integers larger than 0. You can mutate 
+// the Fighter objects.
+// Your function also receives a third argument, a string, 
+// with the name of the fighter that attacks first.
+// Example:
+//     declare_winner(Fighter("Lew", 10, 2), 
+//         Fighter("Harry", 5, 4), "Lew") => "Lew"
+//     Lew attacks Harry; Harry now has 3 health.
+//     Harry attacks Lew; Lew now has 6 health.
+//     Lew attacks Harry; Harry now has 1 health.
+//     Harry attacks Lew; Lew now has 2 health.
+//     Lew attacks Harry: Harry now has -1 health and is 
+//         dead. Lew wins. 
+//     function Fighter(name, health, damagePerAttack) {
+//         this.name = name;
+//         this.health = health;
+//         this.damagePerAttack = damagePerAttack;
+//         this.toString = function() { return this.name; }
+//     }
+
+function declareWinner(fighter1, fighter2, firstAttacker) { 
+    let damage1 = Math.ceil(fighter1.health / fighter2.damagePerAttack); 
+    let damage2 = Math.ceil(fighter2.health / fighter1.damagePerAttack);
+    if (damage1 > damage2) {
+        return fighter1.name;
+    } else if (damage1 < damage2) {
+        return fighter2.name;
+    } 
+    return firstAttacker;
+}
