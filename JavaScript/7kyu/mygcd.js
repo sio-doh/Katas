6c13071c4a28ef108fb3ce2d242fc1aba821da1b
Siobhan Doherty <siobhandoherty18@gmail.com>
2023-06-01 23:56:12 +0200

    Greatest common divisor - js

---
+++ JavaScript/7kyu/mygcd.js
@@ -0,0 +1,10 @@
+// https://www.codewars.com/kata/5500d54c2ebe0a8e8a0003fd
+// DESCRIPTION:
+// Find the greatest common divisor of two positive 
+// integers. The integers can be large, so you need to 
+// find a clever solution.
+// The inputs x and y are always greater or equal to 1, 
+// so the greatest common divisor will always be an 
+// integer that is also greater or equal to 1.
+
+const mygcd = (x, y) => y == 0 ? x : mygcd(y, (x % y))
