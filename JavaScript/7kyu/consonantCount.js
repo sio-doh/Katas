87d2afae8a8de61ff19bc2a430b456e7a4788328
Siobhan Doherty <siobhandoherty18@gmail.com>
2023-06-21 17:04:56 +0200

    Count consonants - js

---
+++ JavaScript/7kyu/consonantCount.js
@@ -0,0 +1,18 @@
+// https://www.codewars.com/kata/564e7fc20f0b53eb02000106
+// DESCRIPTION:
+// Complete the function that takes a string of 
+// English-language text and returns the number of 
+// consonants in the string.
+// Consonants are all letters used to write English excluding 
+// the vowels a, e, i, o, u.
+
+function consonantCount(str) {
+    let zeroVowels = "bcdfghjklmnpqrstvwxyz";
+    let countConsonants = 0; 
+    str.split("").forEach(char => {
+        if (zeroVowels.includes(char.toLowerCase())) {
+            countConsonants = countConsonants + 1;
+        }
+    });
+    return countConsonants;
+}
