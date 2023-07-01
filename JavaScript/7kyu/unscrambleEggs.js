ed57f70bd0914f4e8ebcfb113fa00aa38d46933c
Siobhan Doherty <siobhandoherty18@gmail.com>
2023-07-01 13:29:45 +0200

    Unscrambled eggs - js

---
+++ JavaScript/7kyu/unscrambleEggs.js
@@ -0,0 +1,13 @@
+// https://www.codewars.com/kata/55ea5650fe9247a2ea0000a7
+// DESCRIPTION:
+// Unscramble the eggs.
+// The string given to your function has had an "egg" 
+// inserted directly after each consonant. You need to 
+// return the string before it became eggcoded.
+// Example
+//     unscrambleEggs("Beggegeggineggneggeregg"); => "Beginner"
+//     //             "B---eg---in---n---er---"
+// Kata is supposed to be for beginners to practice regular 
+// expressions, so commenting would be appreciated.
+
+const unscrambleEggs = word => word.replace(/egg/g, "");
