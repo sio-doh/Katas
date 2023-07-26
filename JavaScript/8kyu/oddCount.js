e661bffc1678414bf219ad61d281f3017dc88036
Siobhan Doherty <siobhandoherty18@gmail.com>
2023-07-26 21:43:46 +0200

    Count Odd Numbers below n - js

---
+++ JavaScript/8kyu/oddCount.js
@@ -0,0 +1,10 @@
+// https://www.codewars.com/kata/59342039eb450e39970000a6
+// DESCRIPTION:
+// Given a number n, return the number of positive odd 
+// numbers below n, EASY!
+// Examples (Input -> Output)
+//     7  -> 3 (because odd numbers below 7 are [1, 3, 5])
+//     15 -> 7 (because odd numbers below 15 are [1, 3, 5, 7, 9, 11, 13])
+// Expect large Inputs!
+
+const oddCount = (n) => Math.floor(n / 2)
