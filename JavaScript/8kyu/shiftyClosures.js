bee07563ce0c9f5f6c51600e2ddcc89e4ef5d601
Siobhan Doherty <siobhandoherty18@gmail.com>
2023-07-29 21:23:20 +0200

    Shifty Closures - js

---
+++ JavaScript/8kyu/shiftyClosures.js
@@ -0,0 +1,8 @@
+// https://www.codewars.com/kata/514aa0dc21607ae236000017
+// DESCRIPTION:
+// Functional closures can get overly attached. Set them 
+// straight!
+// Why doesn't greet_abe() actually greet Abe?
+
+var greet_abe = () => "Hello, Abe!"
+var greet_ben = () => "Hello, Ben!"
