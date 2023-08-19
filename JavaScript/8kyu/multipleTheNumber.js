4b2b76b59a647667a3b459ad4dbd5ba6cf16ba35
Siobhan Doherty <siobhandoherty18@gmail.com>
2023-08-19 23:20:00 +0200

    Multiply the number - js

---
+++ JavaScript/8kyu/multipleTheNumber.js
@@ -0,0 +1,14 @@
+// https://www.codewars.com/kata/5708f682c69b48047b000e07
+// DESCRIPTION:
+// Jack really likes his number five: the trick here is that 
+// you have to multiply each number by 5 raised to the number 
+// of digits of each numbers, so, for example:
+//     multiply(3)==15 // 3 * 5¹
+//     multiply(10)==250 // 10 * 5²
+//     multiply(200)==25000 // 200 * 5³
+//     multiply(0)==0 // 0 * 5¹
+//     multiply(-3)==-15 // -3 * 5¹
+
+function multiply(number) {
+    return number * 5 ** String(Math.abs(number)).length;
+}
