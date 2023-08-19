d8850f3b82f032881b268c1f713203fcb17efb85
Siobhan Doherty <siobhandoherty18@gmail.com>
2023-08-19 23:45:41 +0200

    Holiday III - Fire on the boat - py

---
+++ Python/7kyu/fire_fight.py
@@ -0,0 +1,11 @@
+# https://www.codewars.com/kata/57e8fba2f11c647abc000944
+# DESCRIPTION:
+# Enjoying your holiday, you head out on a scuba diving trip!
+# Disaster!! The boat has caught fire!!
+# You will be provided a string that lists many boat related 
+# items. If any of these items are "Fire" you must spring into 
+# action. Change any instance of "Fire" into "~~". Then 
+# return the string.
+
+def fire_fight(s):
+    return s.replace("Fire", "~~")
