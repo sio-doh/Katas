cb178e1e48cd2f359d1eef0e7f8eaa0297477713
Siobhan Doherty <siobhandoherty18@gmail.com>
2023-09-04 23:27:13 +0200

    FIXME: Get Full Name - py

---
+++ Python/7kyu/dinglemouse.py
@@ -0,0 +1,17 @@
+# https://www.codewars.com/kata/597c684822bc9388f600010f
+# DESCRIPTION:
+# The code provided is supposed return a person's Full 
+# Name given their first and last names.
+# But it's not working properly.
+# Notes
+# The first and/or last names are never null, but may be 
+# empty.
+# Task
+# Fix the bug so we can all go home early.
+
+class Dinglemouse(object):
+    def __init__(self, first_name, last_name):
+        self.first_name = first_name
+        self.last_name = last_name
+    def get_full_name(self):
+        return (self.first_name + ' ' + self.last_name).strip()
