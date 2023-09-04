b877332765cb8f4783af5d5741d9f067b27c6931
Siobhan Doherty <siobhandoherty18@gmail.com>
2023-09-04 23:28:51 +0200

    Love vs friendship - py

---
+++ Python/7kyu/words_to_marks.py
@@ -0,0 +1,14 @@
+# https://www.codewars.com/kata/59706036f6e5d1e22d000016
+# DESCRIPTION:
+# If　a = 1, b = 2, c = 3 ... z = 26
+# Then l + o + v + e = 54
+# and f + r + i + e + n + d + s + h + i + p = 108
+# So friendship is twice as strong as love :-)
+# Your task is to write a function which calculates the 
+# value of a word based off the sum of the alphabet positions 
+# of its characters.
+# The input will always be made of only lowercase letters and 
+# will never be empty.
+
+def words_to_marks(s):
+    return sum(ord(x)-96 for x in s)
