e005e95a3612bd78ed55c3a816900d99f8bbf715
Siobhan Doherty <siobhandoherty18@gmail.com>
2023-11-24 17:38:12 +0000

    Reversed Strings - py

---
+++ Python/8kyu/solution.py
@@ -0,0 +1,9 @@
+# https://www.codewars.com/kata/5168bb5dfe9a00b126000018
+# DESCRIPTION:
+# Complete the solution so that it reverses the string 
+# passed into it.
+#     'world'  =>  'dlrow'
+#     'word'   =>  'drow'
+
+def solution(string):
+    return string[::-1]
