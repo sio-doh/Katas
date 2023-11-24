1e10c6d0b3786bb7a9624b6ed11d44c510d3715a
Siobhan Doherty <siobhandoherty18@gmail.com>
2023-11-24 17:24:50 +0000

    Square Every Digit - java

---
+++ Java/7kyu/SquareDigit.java
@@ -0,0 +1,26 @@
+// https://www.codewars.com/kata/546e2562b03326a88e000020
+// DESCRIPTION:
+// Welcome. In this kata, you are asked to square every 
+// digit of a number and concatenate them.
+// For example, if we run 9119 through the function, 
+// 811181 will come out, because 92 is 81 and 12 is 1. 
+// (81-1-1-81)
+// Example #2: An input of 765 will/should return 
+// 493625 because 72 is 49, 62 is 36, and 52 is 25. 
+// (49-36-25)
+// Note: The function accepts an integer and returns an 
+// integer.
+
+public class SquareDigit {
+
+    public int squareDigits(int n) {
+        char[] chars = ("" + n).toCharArray(); 
+        String str = "";
+        for (int i = 0; i < chars.length; i++) {
+            int m = Integer.parseInt(String.valueOf(chars[i])); 
+            str = str + (m * m); 
+        }
+        n = Integer.parseInt(str); 
+        return n;
+    }
+}
